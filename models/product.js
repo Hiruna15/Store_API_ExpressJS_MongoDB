324d2cf8ca0353119ad72920e1c2a9365fea0f2d
Hiruna15 <hirunadilmith15@gmail.com>
2024-11-13 13:34:52 +0530

    update the schema

--- models/product.js
+++ models/product.js
@@ -2,9 +2,19 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const productSchema = new Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: [true, "product name must be provided"] },
   qnt: { type: Number, default: 1 },
-  price: { type: Number, required: true },
+  price: { type: Number, required: [true, "product price must be provided"] },
+  featured: { type: Boolean, default: false },
+  rating: { type: Number, default: 4.5 },
+  createdAt: { type: Date, default: Date.now() },
+  company: {
+    type: String,
+    enum: {
+      values: ["ikea", "liddy", "caressa", "marcos"],
+      message: "{VALUE} is not supported",
+    },
+  },
 });
 
 const productModel = mongoose.model("Product", productSchema);
